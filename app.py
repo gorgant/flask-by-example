5ad84c21fde52a7c306035d9e9cf1bf2f064eed4
Greg Ishktar <gorgant@hotmail.com>
2017-11-06 07:37:03 -0500

    Add a print statement that declares the current environment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -17,3 +17,5 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
+
+
